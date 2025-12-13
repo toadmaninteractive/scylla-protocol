e893f1f4348092281636dccd57b501c7f456f146
Vladimir Dronnikov <dronnikov@gmail.com>
2025-12-13 10:19:32 +0300

    add invalid_data to errors

--- elixir.sh
+++ elixir.sh
@@ -11,11 +11,16 @@ mkdir -p $DIR
 mono bin/igorc.exe -d -v -t elixir \
   -set experimental_server=true \
   -set experimental_client=true \
-    -x gen_elixir/gen_db.cs \
     -x gen_elixir/gen_example.cs \
     $(echo igor/* | sed 's!igor/!-I igor/!g') \
     -o $DIR \
   igor/**/*.igor
+
+# replacements
+sed -r -i 's|Logger\.warn\(|Logger.warning(|g' $DIR/*.ex
+sed -r -i 's|, do: raise (.*)$|, do: raise(\1)|g' $DIR/*.ex
+
+#    -x gen_elixir/gen_db.cs \
 
 # mono bin/igorc.exe -d -v -t postman \
 #     -x postman.cs \
